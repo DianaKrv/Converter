cb7e28b761ffc69ad0b9497847656f4c542d60ff
= <=>
2022-03-04 18:15:43 +0300

    Commit 2. Добавлена функция выбора системы. А также проверка выбора.

--- mainTest.py
+++ mainTest.py
@@ -1,8 +1,10 @@
 import unittest
-
+from main import *
 
 class MyTestCase(unittest.TestCase):
-
+    def test_choice(self):
+        self.assertEqual("Выбрано 1: Ru", choice(1))
+        self.assertEqual("Выбрано 2: Eng", choice(2))
 
 
 
