04aad5b413a9a8fd26e01b8699cf0ada8d834ff5
= <=>
2022-03-04 18:15:33 +0300

    Commit 2. Добавлена функция выбора системы. А также проверка выбора.

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+var = int(input('1: Ru - Eng\n2: Eng - Ru\n'))
+
+def choice(var):
+    if var == 1:
+        print("Выбрано 1: Ru")
+        # def convertToRu()
+    elif var == 2:
+        print("Выбрано 2: Eng")
+        # def convertToEng()
