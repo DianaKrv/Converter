c75e60cd7a686d52df17f0db52acda044c0e518e
= <=>
2022-03-04 18:29:09 +0300

    Commit 3. Выбор подтипа системы. Тестирование выбора.

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 
 
 
+def convertToRu():
+    convertation = int(input('Что считаем: \n 1. Температура\n 2. Вес\n 3. Длина\n 4. Объем\n 5. Скорость\n '))
+    #print(convertation)
 
+
+def convertToEng():
+    print('Конвертация из неметрической в метрическую')
+    convertation = int(input('Что считаем: \n 1. Температура\n 2. Вес\n 3. Длина\n 4. Объем\n 5. Скорость\n '))
+    #print(convertation)
 
 
 
@@ -13,8 +21,8 @@ var = int(input('1: Ru - Eng\n2: Eng - Ru\n'))
 
 def choice(var):
     if var == 1:
-        print("Выбрано 1: Ru")
-        # def convertToRu()
+        #print("Выбрано 1: Ru")
+        convertToRu()
     elif var == 2:
-        print("Выбрано 2: Eng")
-        # def convertToEng()
+        #print("Выбрано 2: Eng")
+        convertToEng()
